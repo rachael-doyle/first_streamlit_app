229a15d84e3088b0f7bb6024c328c3092978899e
Rachael Doyle <106821368+rachael-doyle@users.noreply.github.com>
2022-06-06 15:52:05 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -21,18 +21,15 @@ streamlit.dataframe(fruits_to_show)
 
 
 #new section to display fruityvice api response 
-import requests
-fruityvice_response = requests.get("https://www.fruityvice.com/api/fruit/watermelon")
-
-#take the json version of the response and normalize it 
-fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
-#output it the screen as a table 
-streamlit.dataframe(fruityvice_normalized)
-
-#New section to display fruityvice api response
 streamlit.header('Fruityvice Fruit Advice!')
 fruit_choice = streamlit.text_input('What fruit would you like information about?', 'Kiwi')
 streamlit.write('The user entered', fruit_choice)
 
 import requests 
 fruityvice_response = requests.get('https://fruityvice.com/api/fruit' + fruit_choice)
+
+#take the json version of the response and normalize it 
+fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
+#output it the screen as a table 
+streamlit.dataframe(fruityvice_normalized)
+
